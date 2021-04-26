e97c480af25db4ff0bc7c21cab3c43a2414d1ad1
sergey-as <sergey_as@ukr.net>
2021-04-26 12:51:43 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+// https://www.youtube.com/watch?v=ArmI_skIXpo
+// React hooks-2021-04-7
+
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        //
+      </div>
+  );
+}
+
+export default App; 
